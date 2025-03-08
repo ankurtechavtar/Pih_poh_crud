566d4198c6b512d2b5c8e9dfca8ae743b1cd59dc
Ankur parashar <parasharankurdbg@gmail.com>
2025-03-08 12:49:43 +0530

    third commit

--- Pih_poh_apis/settings.py
+++ Pih_poh_apis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ol_-=&@qn=w_tsd%o90benc(fb(gho@sgjd-=3hnv96_0qqoy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = fALSE
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pih-poh.onrender.com']
 
 
 # Application definition
